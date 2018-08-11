ca5e3539803bbee35e2553d54d3406ec80663b85
Mark Abbott <mark.abbott@ergspace.com>
2018-08-11 22:13:41 +0100

    Define initial set of steps, Before and After hooks for working in a browser

---
+++ src/test/java/resume/BrowserSteps.java
@@ -0,0 +1,51 @@
+package resume;
+
+import cucumber.api.Scenario;
+import cucumber.api.java.After;
+import cucumber.api.java.Before;
+import cucumber.api.java.en.Given;
+import cucumber.api.java.en.When;
+import cucumber.api.java.en.Then;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
+import org.openqa.selenium.WebDriver;
+
+import static org.junit.Assert.*;
+
+
+public class BrowserSteps {
+
+    protected State state;
+
+    public BrowserSteps(resume.State _state) {
+        state = _state;
+    }
+
+    @Before
+    public void startWebDriver() {
+        state.getDriver();
+    }
+
+    @After
+    public void stopWebDriver(Scenario scenario) {
+
+        WebDriver driver = state.getDriver();
+
+
+        if (scenario.isFailed()) {
+            if(driver instanceof TakesScreenshot) {
+                byte[] screenshot = ((TakesScreenshot) driver).getScreenshotAs(OutputType.BYTES);
+                scenario.embed(screenshot, "image/png");
+            }
+        }
+
+        driver.close();
+        driver.quit();
+    }
+
+    @Given("^I am on the homepage$")
+    public void navigateToHomepage() {
+        WebDriver driver = state.getDriver();
+        driver.get("http://localhost:8080");
+    }
+}
